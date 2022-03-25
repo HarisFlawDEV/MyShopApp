b90e6d9bae9a2ab0d812ffa183abd2156386cfac
Haris <harisxstudy@gmail.com>
2022-03-25 21:37:37 +0500

    9th commit

---
+++ app/src/main/java/com/nst/myshopapp/utils/GlideLoader.kt
@@ -0,0 +1,23 @@
+package com.nst.myshopapp.utils
+
+import android.content.Context
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.nst.myshopapp.R
+import java.io.IOException
+
+class GlideLoader(val context: Context) {
+    fun loadUserPicture(imageUri: Uri ,imageView: ImageView){
+        try {
+            Glide
+                .with(context)
+                .load(imageUri)
+                .centerCrop()
+                .placeholder(R.drawable.user)
+                .into(imageView)
+        }catch (e :IOException) {
+            e.printStackTrace()
+        }
+    }
+}
